b1f7722845f5da9fbaa979b2a957ab43cb483def
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2021-03-15 17:45:13 +0200

    Refactor pro spider
    PlayerController works with the pro spider scripts

--- Assets/Scripts/Player/PlayerState/FallingState.cs
+++ Assets/Scripts/Player/PlayerState/FallingState.cs
@@ -6,48 +6,33 @@ namespace SpiderSim.Player.PlayerState
 	public class FallingState : IPlayerState
 	{
 		private PlayerController _player;
-		private Transform _body;
-		private bool _jumped;
-
-		public FallingState(bool jumped = false)
-		{
-			_jumped = jumped;
-		}
+		private Spider _spider;
 
 		public IPlayerState Update(PlayerInput input)
 		{
-			Vector3 origin = _body.TransformPoint(_body.up * _player.sphereCastOffset);
-			LayerMask ownLayer = _player.gameObject.layer;
-
-			Collider[] cols = Physics.OverlapSphere(origin, _player.sphereCastRadius, ownLayer);
-
-			foreach (var collider in cols)
+			if (input.Jump == PlayerInput.Button.Up)
 			{
-				// TODO detect a surface
+				return new MovingState();
 			}
-			
+
 			return null;
+		}
+
+		public void FixedUpdate()
+		{
+			return;
 		}
 
 		public void OnStateEnter(PlayerController player)
 		{
 			Debug.Log("Entering falling state");
 			_player = player;
-			_body = player.body;
-			_player.rb.useGravity = true;
-			_player.rb.isKinematic = false;
-
-			if (_jumped)
-			{
-				Vector3 jumpDirection = _body.transform.TransformDirection(_body.transform.up);
-				_player.rb.AddForce(jumpDirection * _player.jumpForce);
-			}
+			_spider = player.spider;
+			_spider.setGroundcheck(false);
 		}
 
 		public void OnStateExit(PlayerController player)
 		{
-			player.rb.useGravity = false;
-			player.rb.isKinematic = true;
 			Debug.Log("Exiting falling state");
 		}
 	}
--- Assets/Scripts/Player/PlayerState/IPlayerState.cs
+++ Assets/Scripts/Player/PlayerState/IPlayerState.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using UnityEngine;
 
 namespace SpiderSim.Player.PlayerState
@@ -5,6 +6,7 @@ namespace SpiderSim.Player.PlayerState
 	public interface IPlayerState
 	{
 		public IPlayerState Update(PlayerInput input);
+		public void FixedUpdate();
 		public void OnStateEnter(PlayerController player);
 		public void OnStateExit(PlayerController player);
 	}
